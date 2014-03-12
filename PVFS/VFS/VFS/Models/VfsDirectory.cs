1c3243989b75f36b364eff2996490666661ea639
Canandre <canandre@student.ethz.ch>
2014-03-12 15:31:35 +0100

    Added isDirectory() to VfsEntry Changed List<Block> to List<VfsEntry> in VfsDirectory Implemented some methods in VfsDirectory

--- PVFS/VFS/VFS/Models/VfsDirectory.cs
+++ PVFS/VFS/VFS/Models/VfsDirectory.cs
@@ -12,30 +12,74 @@ namespace VFS.VFS.Models
  * Name 110
  * Children x * 4
  */
+
+    //TODO: GetFile-Functions could use NrOfChildren instead of Elements.Count
     class VfsDirectory : VfsFile
     {
-        public List<Block> Children { get; set; }
-
-        public VfsEntry GetElements() {
-            
-        }
+        //Changed from List<Blocks>
+        public List<VfsEntry> Elements { get; set; }
 
         public List<VfsFile> GetFiles() {
-            
+            List<VfsFile> result = new List<VfsFile>();
+            if (Elements.Count > 0) {
+                foreach (VfsEntry element in Elements)
+                {
+                    if (!element.isDirectory) {
+                        result.Add((VfsFile) element);
+                    }
+                }
+            }
+            return result;
         }
 
-        public VfsFile GetFile(string name) {
-            
+        
+        public VfsFile GetFileIgnoringSubDirectories(string name) {
+            if (Elements.Count > 0) {
+                foreach (VfsEntry element in Elements) {
+                    if (!element.isDirectory) {
+                        VfsFile tmp = (VfsFile) element;
+                        if (tmp.Name.Equals(name)) {
+                            return (VfsFile) element;
+                        }
+                    }
+                }
+            }
+            //Size was 0 or no file has been found
+            //TODO: how to handle this exception?
+            return null;
         }
 
+        public VfsFile GetFileCheckingSubDirectories ( string name ) {
+            VfsFile result;
+            if (Elements.Count > 0) {
+                foreach (VfsEntry element in Elements) {
+                    if (element.isDirectory) {
+                        VfsDirectory dir = (VfsDirectory) element;
+                        result = dir.GetFileCheckingSubDirectories(name);
+                        if (result != null)
+                            return result;
+                    } else {
+                        VfsFile tmp = (VfsFile) element;
+                        if (tmp.Name.Equals(name)) {
+                            result = (VfsFile) element;
+                            return result;
+                        }
+                    }
+                }
+            }
+            //Size was 0 or no file has been found
+            //TODO: how to handle this exception?
+            return null;
+        }
+
+        //TODO: might throw exception
         public void AddElement(VfsEntry element) {
-            
+            Elements.Add(element);
         }
 
+        //TODO: might throw excpetion
         public void RemoveElement(VfsEntry element) {
-            
+            Elements.Remove(element);
         }
-
-
     }
 }
--- PVFS/VFS/VFS/Models/VfsEntry.cs
+++ PVFS/VFS/VFS/Models/VfsEntry.cs
@@ -4,6 +4,7 @@ namespace VFS.VFS.Models {
     public abstract class VfsEntry {
         public int Address { get; set; }
 
+        public bool isDirectory { get; set; }
         public static VfsFile Open() {
             throw new NotImplementedException();
         }
