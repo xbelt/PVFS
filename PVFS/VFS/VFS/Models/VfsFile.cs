c6d8112f16c34f473c572dcea47d35070de7e9aa
Canandre <canandre@student.ethz.ch>
2014-03-12 16:42:42 +0100

    not sure... removed empty lines?

--- PVFS/VFS/VFS/Models/VfsFile.cs
+++ PVFS/VFS/VFS/Models/VfsFile.cs
@@ -15,9 +15,7 @@ namespace VFS.VFS.Models
      */
     class VfsFile : VfsEntry
     {
-        public int FileSize { get; set; }
         public List<Block> Inodes { get; set; }
-
         public bool isDirectory { get; set; }
         public VfsDirectory Parent { get; set; }
         public string Name { get; set; }
