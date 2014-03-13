65801422553d8d21628ef2b82c552e79a39fec70
Lukas Häfliger <haelukas@student.ethz.ch>
2014-03-13 17:04:22 +0100

    implement createDisk

--- PVFS/VFS/VFS/DiskFactory.cs
+++ PVFS/VFS/VFS/DiskFactory.cs
@@ -1,11 +1,62 @@
 ﻿using System;
+using System.IO;
 using VFS.VFS.Models;
 
 namespace VFS.VFS
 {
     class DiskFactory : Factory
     {
-        public VfsDisk create(DiskInfo info) {
+        public VfsDisk create(DiskInfo info)
+        {
+            VfsDisk disk = new VfsDisk(info.Path, new DiskProperties{BlockSize = info.BlockSize, MaximumSize = info.Size, Name = info.Name, NumberOfBlocks = (int)Math.Ceiling(info.Size/info.BlockSize), NumberOfUsedBlocks = 1});
+            if (Directory.Exists(info.Path))
+            {
+                var stream = File.Open(info.Path + "\\" + info.Name, FileMode.Create, FileAccess.ReadWrite);
+                disk.FileStream = stream;
+                var writer = new BinaryWriter(stream);
+
+                //BitMap + RootAddress + #Block + #UsedBlocks + Size + NameLength + Name
+                var numberOfUsedBitsInPreamble = disk.DiskProperties.NumberOfBlocks + 4 + 4 + 4 + 8 + 4 + 4*128;
+                var blocksUsedForPreamble = (int)Math.Ceiling((double)numberOfUsedBitsInPreamble / (disk.DiskProperties.BlockSize*8));
+                //write bitMap
+                byte firstByte = 0;
+                for (int i = 0; i <= blocksUsedForPreamble; i++)
+                {
+                    firstByte += (byte) Math.Pow(2, 7 - i);
+                }
+                byte zero = 0;
+                writer.Write(firstByte);
+
+                for (int i = 1; i < Math.Ceiling(disk.DiskProperties.NumberOfBlocks/4d); i++)
+                {
+                    writer.Write(zero);
+                }
+                writer.Flush();
+                //Write root address
+                writer.Write(blocksUsedForPreamble);
+                writer.Write(disk.DiskProperties.NumberOfBlocks);
+                writer.Write(disk.DiskProperties.NumberOfUsedBlocks);
+                writer.Write(disk.DiskProperties.MaximumSize);
+                if (disk.DiskProperties.Name.Length > 128)
+                {
+                    disk.DiskProperties.Name = disk.DiskProperties.Name.Substring(0, 128);
+                }
+                writer.Write(disk.DiskProperties.Name.Length);
+                writer.Write(disk.DiskProperties.Name.ToCharArray());
+                writer.Flush();
+            }
+            else
+            {
+                throw new InvalidPathException(info + " is not a valid path");
+            }
+            return disk;
+        }
+    }
+
+    internal class InvalidPathException : Exception
+    {
+        public InvalidPathException(string s)
+        {
             throw new NotImplementedException();
         }
     }
@@ -17,23 +68,17 @@ namespace VFS.VFS
         private int _blockSize = 2048;
 
         public DiskInfo(string path, string name, double size, int blockSize) {
-            _path = path;
-            _name = name;
-            _size = size;
-            _blockSize = blockSize;
+            Path = path;
+            Name = name;
+            Size = size;
+            BlockSize = blockSize;
         }
 
         public string Path { get; set; }
 
-        public string Name {
-            get { return _name; }
-            set { _name = value; }
-        }
+        public string Name { get; set; }
 
-        public double Size {
-            get { return _size; }
-            set { _size = value; }
-        }
+        public double Size { get; set; }
 
         public int BlockSize {
             get { return _blockSize; }
