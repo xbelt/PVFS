17cfd419ebf8f0440bfdf8aa2f1dcdb4c6b4619e
Lukas Häfliger <haelukas@student.ethz.ch>
2014-03-13 13:38:15 +0100

    some basic implementation to test parser

--- PVFS/VFS/VFS/VFSManager.cs
+++ PVFS/VFS/VFS/VFSManager.cs
@@ -7,9 +7,21 @@ namespace VFS.VFS
 {
     class VFSManager {
         private static List<VfsDisk> _disks;
+        private static VfsDirectory workingDirectory;
 
         public static List<VfsEntry> ls(string path) {
             throw new NotImplementedException();
-        } 
+        }
+
+        public static List<VfsEntry> ls()
+        {
+            Console.WriteLine("LS");
+            return null;
+        }
+
+        public static void cd(string path)
+        {
+            Console.WriteLine("new path: " + path);
+        }
     }
 }
