6722d435f13414273d4f89b2d70a2fc11199bffc
Lukas Häfliger <haelukas@student.ethz.ch>
2014-03-13 18:39:33 +0100

    start on Load method

--- PVFS/VFS/VFS/Models/DiskProperties.cs
+++ PVFS/VFS/VFS/Models/DiskProperties.cs
@@ -8,9 +8,13 @@ namespace VFS.VFS.Models {
         public int NumberOfUsedBlocks { get; set; }
         public double MaximumSize { get; set; }
         public int BlockSize { get; set; }
+        public int RootAddress { get; set; }
 
         public static DiskProperties Load(BinaryReader reader) {
-            throw new NotImplementedException();
+            var dp = new DiskProperties();
+            var buffer = new byte[28];
+            reader.Read(buffer, 0, 28);
+            dp.RootAddress = BitConverter.ToInt32(buffer, 0);
         }
 
         public static void Write(BinaryWriter path) {
