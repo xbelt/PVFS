e218db0c7da98879b266f99d82a06953d798764f
Lukas Häfliger <haelukas@student.ethz.ch>
2014-03-13 14:43:57 +0100

    make Open overrideable

--- PVFS/VFS/VFS/Models/VfsEntry.cs
+++ PVFS/VFS/VFS/Models/VfsEntry.cs
@@ -3,7 +3,7 @@
 namespace VFS.VFS.Models {
     public abstract class VfsEntry {
         public int Address { get; set; }
-        public VfsFile Open(string path) {
+        public virtual VfsFile Open(string path) {
             throw new NotImplementedException();
         }
     }
