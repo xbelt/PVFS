7b77a7760cf648ee88a4afd7a8d4f4ed5e7121c4
Lukas Häfliger <haelukas@student.ethz.ch>
2014-03-13 13:05:24 +0100

    add test (parses input and outputs parsetree)

--- PVFS/VFS/Program.cs
+++ PVFS/VFS/Program.cs
@@ -1,9 +1,24 @@
-﻿namespace VFS
+﻿using System;
+using Antlr4.Runtime;
+using VFS.VFS.Parser;
+
+namespace VFS
 {
     class Program
     {
         static void Main(string[] args)
         {
+            while (true)
+            {
+                var value = Console.ReadLine();
+                AntlrInputStream input = new AntlrInputStream(value);
+                ShellLexer lexer = new ShellLexer(input);
+                CommonTokenStream tokens = new CommonTokenStream(lexer);
+                ShellParser parser = new ShellParser(tokens);
+                var result = parser.compileUnit();
+
+                Console.WriteLine(result.ToStringTree(parser));
+            }
         }
     }
 }
