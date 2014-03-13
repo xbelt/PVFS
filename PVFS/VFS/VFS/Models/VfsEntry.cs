2a03b2f58cad79bd3b4ebda208adafa0b0a522d0
Lukas Häfliger <haelukas@student.ethz.ch>
2014-03-13 15:20:21 +0100

    change return type of open and add overloead whihc accepts address

--- PVFS/VFS/VFS/Models/VfsEntry.cs
+++ PVFS/VFS/VFS/Models/VfsEntry.cs
@@ -3,7 +3,12 @@
 namespace VFS.VFS.Models {
     public abstract class VfsEntry {
         public int Address { get; set; }
-        public virtual VfsFile Open(string path) {
+        public virtual void Open(string path) {
+            throw new NotImplementedException();
+        }
+
+        public virtual void Open(int address)
+        {
             throw new NotImplementedException();
         }
     }
