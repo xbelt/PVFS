3b1d14d6a9248a6ef785eedeac351f217a5a8f61
Lukas Häfliger <haelukas@student.ethz.ch>
2014-03-13 15:19:04 +0100

    add constructor

--- PVFS/VFS/VFS/Models/Block.cs
+++ PVFS/VFS/VFS/Models/Block.cs
@@ -2,6 +2,13 @@
 
 namespace VFS.VFS.Models {
     public class Block {
+        public Block(int address, int startBlock, Block nextBlock)
+        {
+            Address = address;
+            StartBlock = startBlock;
+            NextBlock = nextBlock;
+        }
+
         public int Address { get; set; }
         public int StartBlock { get; set; }
         public Block NextBlock { get; set; }
