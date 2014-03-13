b1d1cac66e0d4f7938a4445f31e0b0f2a4220044
Lukas Häfliger <haelukas@student.ethz.ch>
2014-03-13 13:37:51 +0100

    add listener

---
+++ PVFS/VFS/VFS/Parser/Executor.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace VFS.VFS.Parser
+{
+    class Executor : ShellBaseListener
+    {
+        public override void EnterLs(ShellParser.LsContext context)
+        {
+            VFSManager.ls();
+        }
+
+        public override void EnterCd(ShellParser.CdContext context)
+        {
+            VFSManager.cd(context.Path().Symbol.Text);
+        }
+    }
+}
