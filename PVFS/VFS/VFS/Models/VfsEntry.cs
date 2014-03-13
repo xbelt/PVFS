ef8151706320df1b23f8d65f5506ae98980be4b6
Lukas Häfliger <haelukas@student.ethz.ch>
2014-03-13 14:07:53 +0100

    add path to VfsEntry.Open and remove static modifier

--- PVFS/VFS/VFS/Models/VfsEntry.cs
+++ PVFS/VFS/VFS/Models/VfsEntry.cs
@@ -3,7 +3,7 @@
 namespace VFS.VFS.Models {
     public abstract class VfsEntry {
         public int Address { get; set; }
-        public static VfsFile Open() {
+        public VfsFile Open(string path) {
             throw new NotImplementedException();
         }
     }
