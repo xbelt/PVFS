2ea415f2ee09d813baff907d38d3bfa0d2692fdc
Fabian Meier <meiefabi@student.ethz.ch>
2014-03-22 12:01:55 +0100

    Added VfsConsole as an interface to the console (which we can overwrite in Part 2 of the project)

---
+++ PVFS/VFS/VFS/VfsConsole.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VFS.VFS
+{
+    class VfsConsole
+    {
+
+        public virtual void Message(string Message)
+        {
+            Console.WriteLine(Message);
+        }
+
+        public virtual int Qurey(string Message, params string[] options)
+        {
+            Console.WriteLine(Message);
+            string answer;
+            do
+            {
+                answer = Console.ReadLine();
+            } while (!options.Contains(answer));
+            return options.ToList().IndexOf(answer);
+        }
+    }
+}
