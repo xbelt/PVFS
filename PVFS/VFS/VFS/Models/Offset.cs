672ec079ab92a3a0af13f325fadb1dd884b1c357
Lukas Häfliger <haelukas@student.ethz.ch>
2014-03-24 22:19:25 +0100

    rename Offset class

---
+++ PVFS/VFS/VFS/Models/Offset.cs
@@ -0,0 +1,13 @@
+﻿namespace VFS.VFS.Models
+{
+    static class Offset
+    {
+        public const int NextBlock = 0;
+        public const int StartBlock = 4;
+        public const int FileSize = 8;
+        public const int NumberOfBlocks = 12;
+        public const int IsDirectory = 16;
+        public const int NameLength = 17;
+        public const int Name = 18;
+    }
+}
