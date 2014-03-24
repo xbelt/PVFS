c8c4c7c1cf7360c0043dcd8762dffe2a4145f317
Lukas Häfliger <haelukas@student.ethz.ch>
2014-03-24 19:45:56 +0100

    add Offsets to store common offsets (remove magic values)

---
+++ PVFS/VFS/VFS/Models/Offsets.cs
@@ -0,0 +1,13 @@
+﻿namespace VFS.VFS.Models
+{
+    static class Offsets
+    {
+        public static readonly int NextBlock = 0;
+        public static readonly int StartBlock = 4;
+        public static readonly int FileSize = 8;
+        public static readonly int NumberOfBlocks = 12;
+        public static readonly int IsDirectory = 16;
+        public static readonly int NameLength = 17;
+        public static readonly int Name = 18;
+    }
+}
