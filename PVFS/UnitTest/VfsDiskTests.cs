ab2f340db982c52ff617fc69349efe1dfb415221
Lukas Häfliger <haelukas@student.ethz.ch>
2014-04-02 13:16:08 +0200

    add VfsDiskTests.cs

---
+++ PVFS/UnitTest/VfsDiskTests.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Specialized;
+using System.Diagnostics;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTest
+{
+    [TestClass]
+    public class VfsDiskTests
+    {
+        [TestMethod]
+        public void TestAllocateOnFullDisk() {
+            string name;
+            string path;
+            var disk = DiskFactoryTests.createTestDisk(out path, out name, 2048, 1024);
+            int address;
+            Debug.Assert(!disk.Allocate(out address));
+            Debug.Assert(address == 0);
+        }
+    }
+}
