a77e0b95577619e001e79ec23f14e442b3c98901
Lukas Häfliger <haelukas@student.ethz.ch>
2014-04-02 14:48:44 +0200

    EntryFactory 100% coverage

--- PVFS/UnitTest/EntryFactoryTests.cs
+++ PVFS/UnitTest/EntryFactoryTests.cs
@@ -1,6 +1,103 @@
-﻿namespace UnitTest
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using VFS.VFS;
+using VFS.VFS.Models;
+
+namespace UnitTest
 {
-    class EntryFactoryTests
+    [TestClass]
+    public class EntryFactoryTests
     {
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void TestCreateFileDiskArgNullException()
+        {
+            EntryFactory.createFile(null, "", 0, null);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void TestCreateFileParentArgNullException()
+        {
+            string path;
+            string name;
+            var disk = DiskFactoryTests.createTestDisk(out path, out name, 1000, 200);
+            EntryFactory.createFile(disk, "", 0, null);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void TestCreateFileNameLengthArgumentException()
+        {
+            string path;
+            string name;
+            var disk = DiskFactoryTests.createTestDisk(out path, out name, 1000, 200);
+            EntryFactory.createFile(disk, "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa", 0, null);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void TestCreateFileNegativeLength()
+        {
+            string path;
+            string name;
+            var disk = DiskFactoryTests.createTestDisk(out path, out name, 1000, 200);
+            EntryFactory.createFile(disk, "aa", -1, null);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void TestCreateFileFullDisk()
+        {
+            string path;
+            string name;
+            var disk = DiskFactoryTests.createTestDisk(out path, out name, 2048, 1024);
+            EntryFactory.createFile(disk, "a", 500, new VfsDirectory(disk, 1, "b", null, 1, 1, 0));
+        }
+
+        [TestMethod]
+        public void TestCreateFileMultipleBlockAllocation() {
+            string path;
+            string name;
+            var disk = DiskFactoryTests.createTestDisk(out path, out name, 1000, 200);
+            EntryFactory.createFile(disk, "a", 400, new VfsDirectory(disk, 1, "b", null, 1, 1, 0));
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void TestCreateDirectoryDiskArgNullException()
+        {
+            EntryFactory.createDirectory(null, "", null);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void TestCreateDirectoryParentArgNullException()
+        {
+            string path;
+            string name;
+            var disk = DiskFactoryTests.createTestDisk(out path, out name, 1000, 200);
+            EntryFactory.createDirectory(disk, "", null);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void TestCreateDirectoryNameLengthArgumentException()
+        {
+            string path;
+            string name;
+            var disk = DiskFactoryTests.createTestDisk(out path, out name, 1000, 200);
+            EntryFactory.createDirectory(disk, "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa", null);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void TestCreateDirectoryFullDisk()
+        {
+            string path;
+            string name;
+            var disk = DiskFactoryTests.createTestDisk(out path, out name, 2048, 1024);
+            EntryFactory.createDirectory(disk, "a", new VfsDirectory(disk, 1, "b", null, 1, 1, 0));
+        }
     }
 }
