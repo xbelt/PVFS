8da747ed9a2c31c24f999b26b96223fbb3ef4a5d
Lukas Häfliger <haelukas@student.ethz.ch>
2014-04-02 22:05:33 +0200

    add 96% coverage on VFSDirectoy

--- PVFS/UnitTest/VfsDirectoryTest.cs
+++ PVFS/UnitTest/VfsDirectoryTest.cs
@@ -1,4 +1,5 @@
 using System.Diagnostics;
+using System.Linq;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using VFS.VFS;
 using VFS.VFS.Models;
@@ -79,6 +80,38 @@ namespace UnitTest
             disk.root.GetFile("");
             disk.root.GetEntry("");
             disk.root.ToString();
+        }
+
+        [TestMethod]
+        public void TestGetDirectory()
+        {
+            string path, name;
+            VfsDisk disk = DiskFactoryTests.createTestDisk(out path, out name, 110 * 160, 160);
+            VFSManager.AddAndOpenDisk(disk);
+
+            VFSManager.CreateDirectory("/" + name + "/a", true);
+            VFSManager.CreateDirectory("/" + name + "/b", true);
+
+            var dir = VFSManager.workingDirectory.GetDirectory("a");
+            Assert.AreEqual(dir.Name, "a");
+
+            var dir2 = VFSManager.workingDirectory.GetDirectory("c");
+            Assert.AreEqual(null, dir2);
+        }
+
+        [TestMethod]
+        public void TestGetFiles()
+        {
+            string path, name;
+            VfsDisk disk = DiskFactoryTests.createTestDisk(out path, out name, 110 * 160, 160);
+            VFSManager.AddAndOpenDisk(disk);
+
+            VFSManager.CreateFile("/" + name + "/a");
+            VFSManager.CreateFile("/" + name + "/b");
+
+            var list = VFSManager.workingDirectory.GetFiles().ToList();
+            Assert.AreEqual(list.Count, 2);
+            Assert.AreEqual(list.Exists(x => x.Name == "a"), true);
         }
     }
 }
