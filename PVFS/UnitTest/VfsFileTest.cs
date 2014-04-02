c42ee2ccc9220050ad93e71568c133995ab6774f
Canandre <canandre@student.ethz.ch>
2014-04-02 15:52:13 +0200

    started with tests for VfsFile class

--- PVFS/UnitTest/VfsFileTest.cs
+++ PVFS/UnitTest/VfsFileTest.cs
@@ -1,6 +1,20 @@
-﻿namespace UnitTest
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using VFS.VFS;
+
+namespace UnitTest
 {
-    class VfsFileTest
+    [TestClass]
+    public class VfsFileTest
     {
+        [TestMethod]
+        public void TestgetType()
+        {   //You have to have a local disk with name b and a file fs.txt in root directory
+            const string path = "C:\\Test\\b.vdi";
+            var disk = DiskFactory.Load(path, "a");
+            Assert.AreNotSame(null, disk);
+            Assert.AreNotSame(null, disk.root);
+            Assert.AreNotSame(null, disk.root.GetFile("fs.txt"));
+            Assert.AreEqual("txt", disk.root.GetFile("fs.txt").Type);
+        }
     }
 }
