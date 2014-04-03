058b622f10a783bb6c3be166f89b6df9fcb43120
Canandre <canandre@student.ethz.ch>
2014-04-03 05:07:57 +0200

    removed GetFreeSpace (was already in Manager, didn't notice)

--- PVFS/VFS/VFS/Models/VfsDisk.cs
+++ PVFS/VFS/VFS/Models/VfsDisk.cs
@@ -59,15 +59,6 @@ namespace VFS.VFS.Models
             set { if (value == null) throw new ArgumentNullException("value"); }
         }
 
-        /// <summary>
-        /// Calculates the number of free bytes of the disk
-        /// </summary>
-        /// <returns>Number of free bytes remaining on this disk</returns>
-        public double GetFreeSpace()
-        {
-            return (DiskProperties.NumberOfBlocks - DiskProperties.NumberOfUsedBlocks) *DiskProperties.BlockSize;
-        }
-
         public BinaryReader GetReader() 
         {
             return _reader;
