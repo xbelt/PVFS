345d8713408aefea29f96550fc761be1fce06978
Lukas Häfliger <haelukas@student.ethz.ch>
2014-04-06 22:50:07 +0200

    add ArrayExtensionsUnitTest.cs

---
+++ PVFS/UnitTest/ArrayExtensionsUnitTest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using VFS.VFS;
+
+namespace UnitTest
+{
+    [TestClass]
+    public class ArrayExtensionsUnitTest
+    {
+        [TestMethod]
+        public void IndexOfFound()
+        {
+            var arr = new string[] { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
+            Assert.AreEqual(arr.IndexOf("3"), 2);
+        }
+
+        [TestMethod]
+        public void IndexOfNotFound()
+        {
+            var arr = new string[] { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
+            Assert.AreEqual(arr.IndexOf("0"), -1);
+        }
+    }
+}
